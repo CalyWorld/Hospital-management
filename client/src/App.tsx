da70d4e9986b588dd8bd46c68ae45db410d51f50
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-13 22:34:04 +0800

    rewrote my route paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
 import DoctorHomePage from "./pages/Doctor/DoctorHomePage";
-import PatientHomePage from "./pages/Patient/PatientHomePage";
+import PatientHomePage from "./components/Patient/PatientHomePage";
 import AdminHomePage from "./pages/Admin/AdminHomePage";
 import { AdminUserProvider } from "./contexts/adminUserContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./errorPage";
-import DoctorsTable from "./pages/Admin/DoctorsTable";
-import PatientsTable from "./pages/Admin/PatientsTable";
 import AdminDashBoard from "./pages/Admin/AdminDashBoard";
 import { DoctorProvider } from "./contexts/doctorUserContext";
+import { PatientProvider } from "./contexts/patientUserContext";
+import { DoctorDetails } from "./pages/Doctor/DoctorDetails";
+
 const router = createBrowserRouter([
   {
     path: "/admin",
     element: <AdminHomePage />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -20,34 +22,37 @@ const router = createBrowserRouter([
       {
         path: "dashboard",
         element: <AdminDashBoard />,
+        children: [
+          {
+            path: "doctor/:doctorId",
+            element: <DoctorDetails />,
+          },
+        ],
       },
       {
         path: "doctors",
-        element: <DoctorsTable />,
+        element: <DoctorHomePage />,
+        children: [
+          {
+            path: "doctor/:doctorId",
+            element: <DoctorDetails />,
+          },
+        ],
       },
       {
         path: "patients",
-        element: <PatientsTable />,
+        element: <PatientHomePage />,
       },
     ],
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/doctor",
-    element: <DoctorHomePage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/",
-    element: <PatientHomePage />,
-    errorElement: <ErrorPage />,
   },
 ]);
 function App() {
   return (
     <AdminUserProvider>
       <DoctorProvider>
-        <RouterProvider router={router} />
+        <PatientProvider>
+          <RouterProvider router={router} />
+        </PatientProvider>
       </DoctorProvider>
     </AdminUserProvider>
   );
