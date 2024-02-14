dc9f71c17aa91a72b0274aedb5c3f573d932a21d
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-14 18:51:53 +0800

    update

--- client/src/pages/Admin/AdminHomePage.tsx
+++
@@ -1,7 +0,0 @@
-import { useAdminUser } from "../../contexts/adminUserContext";
-import AdminLoginPage from "./AdminLoginPage";
-import AdminPage from "./AdminPage";
-export default function AdminHomePage() {
-  const { adminUser } = useAdminUser();
-  return <>{adminUser ? <AdminPage /> : <AdminLoginPage />}</>;
-}
--- client/src/pages/Doctor/DoctorDetails.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router";
-
-export function DoctorDetails() {
-  const { doctorId } = useParams();
-  console.log(doctorId);
-  return <div>welcome to doctor detail</div>;
-}
