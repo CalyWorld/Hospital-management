944506bb89633883b4fd4df170d254f4554821c1
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-14 16:36:18 +0800

    admin context can get doctor and patients using admin api

--- client/src/contexts/adminUserContext.tsx
+++ client/src/contexts/adminUserContext.tsx
@@ -6,7 +6,8 @@ import React, {
   ReactNode,
 } from "react";
 import Cookies from "js-cookie";
-
+import { Doctor } from "./doctorUserContext";
+import { Patient } from "./patientUserContext";
 interface AdminUser {
   username: string;
   password: string;
@@ -16,6 +17,11 @@ interface AdminUser {
 
 interface AdminUserContextProps {
   adminUser: AdminUser | null;
+  doctors: Doctor[] | null;
+  loading: boolean;
+  setDoctor?: React.Dispatch<React.SetStateAction<Doctor[] | null>>;
+  patients: Patient[] | null;
+  setPatient?: React.Dispatch<React.SetStateAction<Patient[] | null>>;
   setAdminUser: React.Dispatch<React.SetStateAction<AdminUser | null>>;
   updateAdminUserDetails: (updatedDetails: AdminUser) => void;
 }
@@ -45,13 +51,50 @@ export const AdminUserProvider: React.FC<AdminUserProviderProps> = ({
     : null;
 
   const [adminUser, setAdminUser] = useState<AdminUser | null>(initialUser);
+  const [doctors, setDoctor] = useState<Doctor[] | null>(null);
+  const [patients, setPatient] = useState<Patient[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    if (adminUser) {
-      Cookies.set("adminUser", JSON.stringify(adminUser), { expires: 29 });
-    } else {
-      Cookies.remove("adminUser");
+    let idCounter1 = 0;
+    let idCounter2 = 0;
+    async function fetchData() {
+      try {
+        if (adminUser) {
+          Cookies.set("adminUser", JSON.stringify(adminUser), { expires: 29 });
+          const [doctorResponse, patientResponse] = await Promise.all([
+            fetch(`${import.meta.env.VITE_API_DOCTOR_API_DETAILS}`, {
+              method: "GET",
+            }),
+            fetch(`${import.meta.env.VITE_API_PATIENT_API_DETAILS}`, {
+              method: "GET",
+            }),
+          ]);
+          if (doctorResponse.ok && patientResponse.ok) {
+            const [doctorData, patientData] = await Promise.all([
+              doctorResponse.json(),
+              patientResponse.json(),
+            ]);
+            const doctorsDataWithId = doctorData.map((doctor: Doctor) => ({
+              ...doctor,
+              id: ++idCounter1,
+            }));
+            const patientsDataWithId = patientData.map((patient: Patient) => ({
+              ...patient,
+              id: ++idCounter2,
+            }));
+            setLoading(false);
+            setDoctor(doctorsDataWithId);
+            setPatient(patientsDataWithId);
+          }
+        } else {
+          Cookies.remove("adminUser");
+        }
+      } catch (err) {
+        console.log("error getting data", err);
+      }
     }
+    fetchData();
   }, [adminUser]);
 
   const updateAdminUserDetails = (updatedDetails: AdminUser) => {
@@ -63,6 +106,9 @@ export const AdminUserProvider: React.FC<AdminUserProviderProps> = ({
 
   const contextValue = {
     adminUser,
+    doctors,
+    patients,
+    loading,
     setAdminUser,
     updateAdminUserDetails,
   };
