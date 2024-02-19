6628562dc94a16648f6e4bd4800bff0b8d8ee024
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-19 20:32:55 +0800

    json handling for username and also working on patient medications

--- server/routes/adminRoute.ts
+++ server/routes/adminRoute.ts
@@ -1,21 +1,20 @@
 import express, { Request, Response } from "express";
 import { Doctor } from "../models/doctor";
 import { Patient } from "../models/patient";
-import { Admin, IAdmin } from "../models/admin";
-import Cookies from "js-cookie";
 import { Appointment } from "../models/appointments";
 import { Treatment } from "../models/treatment";
+import { HealthRecords } from "../models/records";
+import { Medication } from "../models/medication";
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
 
-//get admin user
 router.get(
   "/api/admin",
   asyncHandler(async (req: Request, res: Response) => {
     if (req.isAuthenticated()) {
       try {
         const username = req.user;
-        res.json(username);
+        res.status(200).json(username);
       } catch (err) {
         console.log(err);
       }
@@ -64,6 +63,22 @@ router.get(
 );
 
 router.get(
+  "/api/admin/patient/appointments/:patientId",
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const appointments = await Appointment.find({
+        patient: req.params.patientId,
+      })
+        .populate("doctor")
+        .exec();
+      res.status(200).json(appointments);
+    } catch (err) {
+      console.log(err);
+    }
+  }),
+);
+
+router.get(
   "/api/admin/doctor/treatments/:doctorId",
   asyncHandler(async (req: Request, res: Response) => {
     try {
@@ -73,6 +88,40 @@ router.get(
         .populate("doctor")
         .exec();
       res.status(200).json(treatment);
+    } catch (err) {
+      console.log(err);
+    }
+  }),
+);
+
+router.get(
+  "/api/admin/patient/records/:patientId",
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const records = await HealthRecords.find({
+        patient: req.params.patientId,
+      })
+        .populate("treatments")
+        .exec();
+      res.status(200).json(records);
+    } catch (err) {
+      console.log(err);
+    }
+  }),
+);
+
+router.get(
+  "/api/admin/patient/medications/:medicationId",
+  asyncHandler(async (req: Request, res: Response) => {
+    try {
+      console.log(req.params.medicationId);
+      const patientMedication = await Treatment.find({
+        medication: { $in: [req.params.medicationId] },
+      })
+        .populate("medication")
+        .exec();
+      console.log(patientMedication);
+      // res.status(200).json(patientMedication);
     } catch (err) {
       console.log(err);
     }
