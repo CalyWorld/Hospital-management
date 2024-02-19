758ed10c5b8bbd16570db5754ac195333b990d0e
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-19 20:32:26 +0800

    update

--- client/src/components/pages/Doctor/DoctorHomePage.tsx
+++ client/src/components/pages/Doctor/DoctorHomePage.tsx
@@ -2,8 +2,6 @@ import DoctorsTable from "./DoctorsTable";
 import { Outlet, useLocation } from "react-router";
 export default function DoctorHomePage() {
   const location = useLocation();
-  console.log(location.pathname);
-  console.log("i am in doctorHomePage");
   return (
     <div>
       {location.pathname === "/admin/doctors" ? <DoctorsTable /> : <Outlet />}
