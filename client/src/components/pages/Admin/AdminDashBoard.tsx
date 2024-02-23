4cad5e7ab58448dbaf430cd7486353b3d037f6e4
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-23 17:55:45 +0800

    still working on admin dashboard

--- client/src/components/pages/Admin/AdminDashBoard.tsx
+++ client/src/components/pages/Admin/AdminDashBoard.tsx
@@ -1,10 +1,6 @@
-import DoctorsTable from "../Doctor/DoctorsTable";
-import TuneIcon from "@mui/icons-material/Tune";
 import { FaUserDoctor, FaMoneyBill } from "react-icons/fa6";
 import { FaHospitalUser, FaCalendarAlt } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
-import PatientsTable from "../Patient/PatientsTable";
-import { useState } from "react";
 import { Outlet, useLocation } from "react-router";
 import { useAdminUser } from "../../../contexts/adminUserContext";
 import DateCalendarValue from "../../calendar";
@@ -12,18 +8,32 @@ export default function AdminDashBoard() {
   const { useGetDoctorAndPatientData, useGetTotalRevenue } = useAdminUser();
   const { doctors, patients } = useGetDoctorAndPatientData();
   const { totalRevenue } = useGetTotalRevenue();
-
-  const [switchTable, setTable] = useState<boolean>(false);
   const location = useLocation();
 
   return (
     <>
       {location.pathname === "/admin" || "/admin/dashboard" ? (
-        <div className="flex justify-between h-full gap-10 ">
+        <div className="flex justify-between gap-10 ">
           <div className="left-side-container w-full flex flex-col gap-10">
-            <div className="welcome-info">
-              <p>Welcome</p>
-              <p>Admin</p>
+            <div className="welcome-info relative">
+              <img
+                className="h-96 w-full"
+                src="https://www.mathematica.org/-/media/internet/health-graphics-and-photos/health-graphics/2020/medical_health_interactive_web.jpg"
+                alt="welcome-image"
+              />
+              <div className="absolute left-6 top-24 text-white flex flex-col gap-10">
+                <div>
+                  <p>Welcome</p>
+                  <p>Admin</p>
+                </div>
+                <div>
+                  <p>
+                    To keep the body in good health is a duty ... otherwise
+                    <br />
+                    we shall not be able to keep our mind strong and clear
+                  </p>
+                </div>
+              </div>
             </div>
             <div className="stats-info flex flex-col gap-5">
               <div className="flex justify-between p-5 shadow bg-white rounded-md">
