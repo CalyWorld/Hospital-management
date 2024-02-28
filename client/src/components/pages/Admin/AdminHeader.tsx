439734cda3661241df3e59cf92fd8b78cf84d4bd
CalyWorld <obinnaanizoba5@gmail.com>
2024-02-28 13:53:28 +0800

    fixed path content for dashboard bug

--- client/src/components/pages/Admin/AdminHeader.tsx
+++ client/src/components/pages/Admin/AdminHeader.tsx
@@ -1,8 +1,20 @@
 import PersonIcon from "@mui/icons-material/Person";
+import { useLocation } from "react-router";
 export default function AdminHeader() {
+  const location = useLocation();
+  const getPathContent = () => {
+    if (location.pathname.includes("/admin/doctors")) {
+      return "DOCTORS";
+    } else if (location.pathname.includes("/admin/patients")) {
+      return "PATIENTS";
+    } else {
+      return "DASHBOARD";
+    }
+  };
   return (
-    <header className="p-2">
-      <nav className="flex justify-end">
+    <header className="ml-3 mr-3 mt-0 mb-0">
+      <nav className="flex justify-between items-center shadow bg-white rounded-md p-3">
+        {getPathContent()}
         <PersonIcon fontSize="large" />
       </nav>
     </header>
