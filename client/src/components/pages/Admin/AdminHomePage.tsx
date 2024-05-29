0ce2c7c846cfaffa172ebe9e99630c47ebaa7ab4
callistus <callistus.anizoba@lanexcorp.com>
2024-05-29 20:41:44 +0800

    changed sidebar to layout

--- client/src/components/pages/Admin/AdminHomePage.tsx
+++ client/src/components/pages/Admin/AdminHomePage.tsx
@@ -2,8 +2,6 @@ import { useAdminUser } from "../../../contexts/adminUserContext";
 import AdminLoginPage from "./AdminLoginPage";
 import AdminPage from "./AdminPage";
 export default function AdminHomePage() {
-  const { useGetAdmin } = useAdminUser();
-  const { adminUser } = useGetAdmin();
-
+  const { adminUser } = useAdminUser();
   return adminUser ? <AdminPage /> : <AdminLoginPage />;
 }
---
+++ client/src/components/pages/Admin/AdminLayout.tsx
@@ -0,0 +1,103 @@
+import { Link } from "react-router-dom";
+import HomeIcon from "@mui/icons-material/Home";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import { FaUserDoctor } from "react-icons/fa6";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { FaHospitalUser } from "react-icons/fa";
+import { useState } from "react";
+export default function AdminLayout() {
+  const [sideNavTabLink, setNavTabLink] = useState("");
+  return (
+    <nav
+      id="sidebar"
+      className="w-64 h-screen bg-darkBlue text-white flex flex-col gap-10 p-3"
+    >
+      <ul>
+        <li>
+          <a href="/admin">
+            <HealthAndSafetyIcon />
+          </a>
+        </li>
+      </ul>
+      <div className="h-full flex flex-col justify-between">
+        <ul className="flex flex-col items-center gap-3">
+          <li className="w-full">
+            <Link
+              to={`/admin/dashboard`}
+              className={`${
+                sideNavTabLink === "dashboard"
+                  ? "flex gap-4 p-2 w-full items-center cursor-pointer shadow bg-[#4b5563] rounded-md"
+                  : "flex gap-4 p-2 w-full items-center cursor-pointer hover:shadow rounded-md hover:bg-[#374151]"
+              }`}
+              onClick={() => {
+                setNavTabLink("dashboard");
+              }}
+            >
+              <HomeIcon sx={{ fontSize: 25 }} />
+              <p>Dashboard</p>
+            </Link>
+          </li>
+          <li className="w-full">
+            <Link
+              to={`/admin/doctors`}
+              className={`${
+                sideNavTabLink === "doctors"
+                  ? "flex gap-4 p-2 w-full items-center cursor-pointer shadow bg-[#4b5563] rounded-md"
+                  : "flex gap-4 p-2 w-full items-center cursor-pointer hover:shadow rounded-md hover:bg-[#374151]"
+              }`}
+              onClick={() => {
+                setNavTabLink("doctors");
+              }}
+            >
+              <FaUserDoctor size={23} />
+              <p>Doctor</p>
+            </Link>
+          </li>
+          <li className="w-full">
+            <Link
+              to={`/admin/patients`}
+              className={`${
+                sideNavTabLink === "patients"
+                  ? "flex gap-4 p-2 w-full items-center  cursor-pointer shadow bg-[#4b5563] rounded-md"
+                  : "flex gap-4 p-2 w-full items-center cursor-pointer hover:shadow rounded-md hover:bg-[#374151]"
+              }`}
+              onClick={() => {
+                setNavTabLink("patients");
+              }}
+            >
+              <FaHospitalUser size={23} />
+              <p>Patient</p>
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex flex-col items-center gap-5">
+          <div
+            className={`${
+              sideNavTabLink === "settings"
+                ? "flex gap-5 p-2 w-full items-center justify-center cursor-pointer shadow bg-[#4b5563] rounded-md"
+                : "flex gap-5 p-2 w-full items-center justify-center cursor-pointer hover:shadow rounded-md hover:bg-[#374151]"
+            }`}
+            onClick={() => {
+              setNavTabLink("settings");
+            }}
+          >
+            <SettingsIcon />
+          </div>
+          <div
+            className={`${
+              sideNavTabLink === "logout"
+                ? "flex gap-5 p-2 w-full items-center justify-center cursor-pointer shadow bg-[#4b5563] rounded-md"
+                : "flex gap-5 p-2 w-full items-center justify-center cursor-pointer hover:shadow rounded-md hover:bg-[#374151]"
+            }`}
+            onClick={() => {
+              setNavTabLink("logout");
+            }}
+          >
+            <LogoutIcon />
+          </div>
+        </ul>
+      </div>
+    </nav>
+  );
+}
