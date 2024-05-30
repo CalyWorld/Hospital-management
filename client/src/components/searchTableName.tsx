d05de201748beb7da172770d19a2aada59a5235e
callistus <callistus.anizoba@lanexcorp.com>
2024-05-30 21:15:07 +0800

    fixed searchName bugs to change elements to strings when searching for name

--- client/src/components/searchTableName.tsx
+++ client/src/components/searchTableName.tsx
@@ -5,9 +5,14 @@ export const searchName = (
   setState: React.Dispatch<React.SetStateAction<Row[]>>,
 ) => {
   let value = (e.target as HTMLInputElement).value;
-  const searchedItem = data.filter((table) =>
-    table.username.toLowerCase().includes(value),
-  );
+  const searchedItem = data.filter((table) => {
+    const usernameString =
+      typeof table.username === "string"
+        ? table.username
+        : `${table.username.props.children[0]} ${table.username.props.children[2]}`;
+
+    return usernameString.toLowerCase().includes(value);
+  });
   console.log(searchedItem);
   setState(searchedItem);
 };
