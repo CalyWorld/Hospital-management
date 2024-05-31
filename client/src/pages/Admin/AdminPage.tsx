421d867ae7df84e3e3e258a1a46ff59ab3989b15
callistus <callistus.anizoba@lanexcorp.com>
2024-05-31 21:45:48 +0800

    implemented blur when there is a modal pop up

--- client/src/pages/Admin/AdminPage.tsx
+++ client/src/pages/Admin/AdminPage.tsx
@@ -1,12 +1,33 @@
+import { DoctorsTableProps } from "../Doctor/DoctorsTable";
+import EditDoctorDetail from "../../forms/EditDoctorDetailsForm";
+import DeleteDoctor from "../../forms/DeleteDoctorForm";
+import BookAppointment from "../../forms/BookAppointment";
 import AdminHeader from "./AdminHeader";
 import AdminLayout from "./AdminLayout";
 import { Outlet } from "react-router";
-export default function AdminPage() {
+
+export default function AdminPage({ openActionForm }: DoctorsTableProps) {
   return (
-    <div className="flex bg-white h-screen">
+    <div className="flex h-screen">
       <div className="flex w-full h-full">
-        <AdminLayout />
-        <div id="detail" className="w-full flex flex-col">
+        {openActionForm === "editForm" && <EditDoctorDetail />}
+        {openActionForm === "deleteDoctor" && <DeleteDoctor />}
+        {openActionForm === "bookPatient" && <BookAppointment />}
+        <div
+          className={`${
+            openActionForm === "deleteDoctor"
+              ? "bg-darkBlue blur-[4px]"
+              : "bg-darkBlue"
+          }`}
+        >
+          <AdminLayout />
+        </div>
+        <div
+          id="detail"
+          className={`w-full flex flex-col ${
+            openActionForm === "deleteDoctor" ? "blur-[4px]" : "bg-white"
+          }`}
+        >
           <AdminHeader />
           <div className="flex-grow overflow-y-auto">
             <Outlet />
