91c5b2d7845ee0e0be005f2fe12a1204b1cd7198
callistus <callistus.anizoba@lanexcorp.com>
2024-05-31 22:12:00 +0800

    fixed the modal bugs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import DoctorHomePage from "./pages/Doctor/DoctorHomePage";
 import PatientHomePage from "./pages/Patient/PatientHomePage";
 import AdminHomePage from "./pages/Admin/AdminHomePage";
+import { useState } from "react";
 import { AdminUserProvider } from "./contexts/adminUserContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./errorPage";
@@ -9,123 +10,145 @@ import { DoctorDetails } from "./pages/Doctor/DoctorDetails";
 import PatientsTable from "./pages/Patient/PatientsTable";
 import { PatientDetails } from "./pages/Patient/PatientDetails";
 import DoctorsTable from "./pages/Doctor/DoctorsTable";
+import EditDoctorDetail from "./forms/EditDoctorDetailsForm";
+import DeleteDoctor from "./forms/DeleteDoctorForm";
+import BookAppointment from "./forms/BookAppointment";
 import DoctorScheduledAppointmentTable from "./pages/Doctor/DoctorScheduledAppointmentTable";
 import DoctorCompletedAppointmentTable from "./pages/Doctor/DoctorCompletedAppointmentTable";
 import PatientScheduledAppointmentTable from "./pages/Patient/PatientScheduledAppointmentTable";
 import PatientCompletedAppointmentTable from "./pages/Patient/PatientCompletedAppointmentTable";
 
-const router = createBrowserRouter([
-  {
-    path: "/admin",
-    element: <AdminHomePage />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <AdminDashBoard />,
-      },
-      {
-        path: "dashboard",
-        element: <AdminDashBoard />,
-        children: [
-          {
-            path: "doctor/:doctorId",
-            element: <DoctorDetails />,
-            children: [
-              {
-                index: true,
-                path: "active",
-                element: <DoctorScheduledAppointmentTable />,
-              },
-              {
-                path: "completion",
-                element: <DoctorCompletedAppointmentTable />,
-              },
-              {
-                path: "patients",
-                element: <PatientsTable />,
-              },
-            ],
-          },
-          {
-            path: "patient/:patientId",
-            element: <PatientDetails />,
-            children: [
-              {
-                index: true,
-                path: "active",
-                element: <PatientScheduledAppointmentTable />,
-              },
-              {
-                path: "completion",
-                element: <PatientCompletedAppointmentTable />,
-              },
-              {
-                path: "patients",
-                element: <DoctorsTable />,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "doctors",
-        element: <DoctorHomePage />,
-        children: [
-          {
-            path: "doctor/:doctorId",
-            element: <DoctorDetails />,
-            children: [
-              {
-                index: true,
-                path: "active",
-                element: <DoctorScheduledAppointmentTable />,
-              },
-              {
-                path: "completion",
-                element: <DoctorCompletedAppointmentTable />,
-              },
-              {
-                path: "patients",
-                element: <PatientsTable />,
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "patients",
-        element: <PatientHomePage />,
-        children: [
-          {
-            path: "patient/:patientId",
-            element: <PatientDetails />,
-            children: [
-              {
-                index: true,
-                path: "active",
-                element: <PatientScheduledAppointmentTable />,
-              },
-              {
-                path: "completion",
-                element: <PatientCompletedAppointmentTable />,
-              },
-              {
-                path: "doctors",
-                element: <DoctorsTable />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+function routerFunc() {
+  const [openActionForm, setActionForm] = useState<string>("");
+  const router = createBrowserRouter([
+    {
+      path: "/admin",
+      element: <AdminHomePage openActionForm={openActionForm} />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <AdminDashBoard />,
+        },
+        {
+          path: "dashboard",
+          element: <AdminDashBoard />,
+          children: [
+            {
+              path: "doctor/:doctorId",
+              element: <DoctorDetails />,
+              children: [
+                {
+                  index: true,
+                  path: "active",
+                  element: <DoctorScheduledAppointmentTable />,
+                },
+                {
+                  path: "completion",
+                  element: <DoctorCompletedAppointmentTable />,
+                },
+                {
+                  path: "patients",
+                  element: <PatientsTable />,
+                },
+              ],
+            },
+            {
+              path: "patient/:patientId",
+              element: <PatientDetails />,
+              children: [
+                {
+                  index: true,
+                  path: "active",
+                  element: <PatientScheduledAppointmentTable />,
+                },
+                {
+                  path: "completion",
+                  element: <PatientCompletedAppointmentTable />,
+                },
+                {
+                  path: "patients",
+                  element: <DoctorsTable setActionForm={setActionForm} />,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: "doctors",
+          element: <DoctorHomePage setActionForm={setActionForm} />,
+          children: [
+            {
+              path: "doctor/:doctorId",
+              element: <DoctorDetails />,
+              children: [
+                {
+                  index: true,
+                  path: "active",
+                  element: <DoctorScheduledAppointmentTable />,
+                },
+                {
+                  path: "completion",
+                  element: <DoctorCompletedAppointmentTable />,
+                },
+                {
+                  path: "patients",
+                  element: <PatientsTable />,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          path: "patients",
+          element: <PatientHomePage />,
+          children: [
+            {
+              path: "patient/:patientId",
+              element: <PatientDetails />,
+              children: [
+                {
+                  index: true,
+                  path: "active",
+                  element: <PatientScheduledAppointmentTable />,
+                },
+                {
+                  path: "completion",
+                  element: <PatientCompletedAppointmentTable />,
+                },
+                {
+                  path: "doctors",
+                  element: <DoctorsTable setActionForm={setActionForm} />,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ]);
+  return { router, openActionForm, setActionForm };
+}
+
 function App() {
+  const { router, openActionForm, setActionForm } = routerFunc();
   return (
-    <AdminUserProvider>
-      <RouterProvider router={router} />
-    </AdminUserProvider>
+    <div>
+      <div
+        onClick={() => {
+          if (openActionForm === "deleteDoctor") {
+            setActionForm("");
+          }
+        }}
+      >
+        <AdminUserProvider>
+          <RouterProvider router={router} />
+        </AdminUserProvider>
+      </div>
+      {openActionForm === "editForm" && <EditDoctorDetail />}
+      {openActionForm === "deleteDoctor" && <DeleteDoctor />}
+      {openActionForm === "bookPatient" && <BookAppointment />}
+    </div>
   );
 }
 
