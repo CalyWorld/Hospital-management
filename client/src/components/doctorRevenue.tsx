a23fe667cea31218723f58f34fceefde1f38ecf2
callistus <callistus.anizoba@lanexcorp.com>
2024-06-01 23:18:59 +0800

    implement calculating doctor revenues

---
+++ client/src/components/doctorRevenue.tsx
@@ -0,0 +1,34 @@
+import { Treatment } from "../contexts/adminUserContext";
+import { currentMonth } from "./currentMonth";
+export const doctorRevenue = (doctorTreatments: Treatment[] | null) => {
+  const allTreatmentsFees = doctorTreatments?.map(
+    (treatment) => treatment.totalFee,
+  );
+  const totalRevenueAllTime = allTreatmentsFees?.reduce(
+    (accumulator, currentValue) => accumulator + (currentValue || 0),
+    0,
+  );
+
+  const treatmentsWithinCurrentMonth = doctorTreatments?.filter((treatment) => {
+    const treatmentDate = new Date(treatment.date);
+    if (isNaN(treatmentDate.getTime())) {
+      return false;
+    }
+    return (
+      treatmentDate
+        .toLocaleDateString("en-us", { month: "short" })
+        .toLowerCase() === currentMonth
+    );
+  });
+
+  const totalRevenueCurrentMonth = treatmentsWithinCurrentMonth?.reduce(
+    (accumulator, currentTreatment) =>
+      accumulator + (currentTreatment.totalFee || 0),
+    0,
+  );
+
+  return {
+    totalRevenueAllTime: totalRevenueAllTime || 0,
+    totalRevenueCurrentMonth: totalRevenueCurrentMonth || 0,
+  };
+};
