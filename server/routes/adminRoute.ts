38945c7e794062701a7e0698e2d16552a92b4371
callistus <callistus.anizoba@lanexcorp.com>
2024-06-02 06:17:51 +0800

    refractored

--- server/routes/adminRoute.ts
+++ server/routes/adminRoute.ts
@@ -1,196 +1,68 @@
-import express, { Request, Response } from "express";
-import { Doctor } from "../models/doctor";
-import { Patient } from "../models/patient";
-import { Appointment } from "../models/appointments";
-import { Treatment } from "../models/treatment";
-import { HealthRecords } from "../models/records";
-import { Medication } from "../models/medication";
+import express from "express";
+import AdminController from "../controllers/adminController";
 const router = express.Router();
 const asyncHandler = require("express-async-handler");
-
+const adminController = AdminController;
 router.get(
   "/api/admin",
-  asyncHandler(async (req: Request, res: Response) => {
-    if (req.isAuthenticated()) {
-      try {
-        const username = req.user;
-        res.status(200).json(username);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-  }),
+  asyncHandler(adminController.getAdmin.bind(adminController)),
 );
 
 router.get(
   "/api/admin/doctor",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const doctors = await Doctor.find().exec();
-      res.status(200).json(doctors);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getAllDoctors.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patient",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const patients = await Patient.find().exec();
-      res.status(200).json(patients);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getAllPatients.bind(adminController)),
 );
 
 router.get(
   "/api/admin/totalFees",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const treatmentFees = await Treatment.find()
-        .populate("medication")
-        .exec();
-      res.status(200).json(treatmentFees);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getTotalFees.bind(adminController)),
 );
 
 router.get(
   "/api/admin/doctor/appointments/:doctorId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const appointments = await Appointment.find({
-        doctor: req.params.doctorId,
-      })
-        .populate("patient")
-        .exec();
-      res.status(200).json(appointments);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getDoctorAppointments.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patient/appointments/:patientId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const appointments = await Appointment.find({
-        patient: req.params.patientId,
-      })
-        .populate("doctor")
-        .exec();
-      res.status(200).json(appointments);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getPatientAppointments.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patients/appointments/:date",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const startOfDay = new Date(req.params.date);
-      startOfDay.setUTCHours(0, 0, 0, 0);
-
-      const endOfDay = new Date(req.params.date);
-      endOfDay.setUTCHours(23, 59, 59, 999);
-
-      const appointments = await Appointment.find({
-        startDate: {
-          $gte: startOfDay.toISOString(),
-        },
-        endDate: {
-          $lte: endOfDay.toISOString(),
-        },
-      })
-        .populate("doctor patient")
-        .exec();
-      res.status(200).json(appointments);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(
+    adminController.getPatientAppointmentsByDate.bind(adminController),
+  ),
 );
 
 router.get(
   "/api/admin/doctor/treatments/:doctorId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const treatment = await Treatment.find({
-        doctor: req.params.doctorId,
-      })
-        .populate("doctor")
-        .exec();
-      res.status(200).json(treatment);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getDoctorTreatments.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patient/records/:patientId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const records = await HealthRecords.find({
-        patient: req.params.patientId,
-      })
-        .populate("treatments")
-        .exec();
-      res.status(200).json(records);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getPatientRecords.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patient/medications/:medicationId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const patientMedication = await Medication.findById(
-        req.params.medicationId,
-      ).exec();
-      res.status(200).json(patientMedication);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getPatientMedications.bind(adminController)),
 );
 
 router.get(
   "/api/admin/doctor/:doctorId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const doctorById = await Doctor.findById(req.params.doctorId)
-        .populate("patient")
-        .exec();
-      res.status(200).json(doctorById);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getDoctorDetails.bind(adminController)),
 );
 
 router.get(
   "/api/admin/patient/:patientId",
-  asyncHandler(async (req: Request, res: Response) => {
-    try {
-      const patientById = await Patient.findById(req.params.patientId)
-        .populate("doctor")
-        .exec();
-      res.status(200).json(patientById);
-    } catch (err) {
-      console.log(err);
-    }
-  }),
+  asyncHandler(adminController.getPatientDetails.bind(adminController)),
 );
 
 module.exports = router;
