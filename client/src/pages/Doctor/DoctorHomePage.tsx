d072851a48c7564474901f703743253002d84cc3
callistus <callistus.anizoba@lanexcorp.com>
2024-06-01 17:26:47 +0800

    update

--- client/src/pages/Doctor/DoctorHomePage.tsx
+++ client/src/pages/Doctor/DoctorHomePage.tsx
@@ -1,22 +1,11 @@
-import { useState } from "react";
-import DoctorsTable from "./DoctorsTable";
-import EditDoctorDetail from "../../forms/EditDoctorDetailsForm";
-import DeleteDoctor from "../../forms/DeleteDoctorForm";
-import BookAppointment from "../../forms/BookAppointment";
+import DoctorsTable, { TableProps } from "./DoctorsTable";
 import { Outlet, useLocation } from "react-router";
-export default function DoctorHomePage() {
-  const [openActionForm, setActionForm] = useState<string>("");
+export default function DoctorHomePage({ setActionForm }: TableProps) {
   const location = useLocation();
   return (
-    <div className="m-10 shadow bg-white rounded-md relative">
+    <div className="m-10 shadow rounded-md">
       {location.pathname === "/admin/doctors" ? (
-        <div className="relative">
-          {" "}
-          {openActionForm === "editForm" && <EditDoctorDetail />}
-          {openActionForm === "deleteDoctor" && <DeleteDoctor />}
-          {openActionForm === "bookPatient" && <BookAppointment />}
-          <DoctorsTable setActionForm={setActionForm} />
-        </div>
+        <DoctorsTable setActionForm={setActionForm} />
       ) : (
         <Outlet />
       )}
