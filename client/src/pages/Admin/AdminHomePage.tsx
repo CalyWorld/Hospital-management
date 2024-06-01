23ce0bca36baddf29595fd66cd3edd3f29bc9d0c
callistus <callistus.anizoba@lanexcorp.com>
2024-06-01 17:28:44 +0800

    renamed table interface

--- client/src/pages/Admin/AdminHomePage.tsx
+++ client/src/pages/Admin/AdminHomePage.tsx
@@ -1,7 +1,12 @@
 import { useAdminUser } from "../../contexts/adminUserContext";
+import { TableProps } from "../Doctor/DoctorsTable";
 import AdminLoginPage from "./AdminLoginPage";
 import AdminPage from "./AdminPage";
-export default function AdminHomePage() {
+export default function AdminHomePage({ openActionForm }: TableProps) {
   const { adminUser } = useAdminUser();
-  return adminUser ? <AdminPage /> : <AdminLoginPage />;
+  return adminUser ? (
+    <AdminPage openActionForm={openActionForm} />
+  ) : (
+    <AdminLoginPage />
+  );
 }
