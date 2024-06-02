ef6797fc254f1e0f29ae827d833d86da4d090441
callistus <callistus.anizoba@lanexcorp.com>
2024-06-02 18:24:47 +0800

    grouped the routes better

--- server/routes/adminRoute.ts
+++ server/routes/adminRoute.ts
@@ -1,68 +1,66 @@
 import express from "express";
 import AdminController from "../controllers/adminController";
+import asyncHandler from "express-async-handler";
+
 const router = express.Router();
-const asyncHandler = require("express-async-handler");
 const adminController = AdminController;
+
+// Admin Routes
 router.get(
   "/api/admin",
   asyncHandler(adminController.getAdmin.bind(adminController)),
 );
 
+// Doctor Routes
 router.get(
   "/api/admin/doctor",
   asyncHandler(adminController.getAllDoctors.bind(adminController)),
 );
-
-router.get(
-  "/api/admin/patient",
-  asyncHandler(adminController.getAllPatients.bind(adminController)),
-);
-
-router.get(
-  "/api/admin/totalFees",
-  asyncHandler(adminController.getTotalFees.bind(adminController)),
-);
-
 router.get(
   "/api/admin/doctor/appointments/:doctorId",
   asyncHandler(adminController.getDoctorAppointments.bind(adminController)),
 );
-
-router.get(
-  "/api/admin/patient/appointments/:patientId",
-  asyncHandler(adminController.getPatientAppointments.bind(adminController)),
-);
-
-router.get(
-  "/api/admin/patients/appointments/:date",
-  asyncHandler(
-    adminController.getPatientAppointmentsByDate.bind(adminController),
-  ),
-);
-
 router.get(
   "/api/admin/doctor/treatments/:doctorId",
   asyncHandler(adminController.getDoctorTreatments.bind(adminController)),
 );
-
-router.get(
-  "/api/admin/patient/records/:patientId",
-  asyncHandler(adminController.getPatientRecords.bind(adminController)),
-);
-
-router.get(
-  "/api/admin/patient/medications/:medicationId",
-  asyncHandler(adminController.getPatientMedications.bind(adminController)),
-);
-
 router.get(
   "/api/admin/doctor/:doctorId",
   asyncHandler(adminController.getDoctorDetails.bind(adminController)),
 );
 
+// Patient Routes
+router.get(
+  "/api/admin/patient",
+  asyncHandler(adminController.getAllPatients.bind(adminController)),
+);
+router.get(
+  "/api/admin/patient/appointments/:patientId",
+  asyncHandler(adminController.getPatientAppointments.bind(adminController)),
+);
+router.get(
+  "/api/admin/patient/records/:patientId",
+  asyncHandler(adminController.getPatientRecords.bind(adminController)),
+);
+router.get(
+  "/api/admin/patient/medications/:medicationId",
+  asyncHandler(adminController.getPatientMedications.bind(adminController)),
+);
 router.get(
   "/api/admin/patient/:patientId",
   asyncHandler(adminController.getPatientDetails.bind(adminController)),
+);
+
+// General Routes
+router.get(
+  "/api/admin/totalFees",
+  asyncHandler(adminController.getTotalFees.bind(adminController)),
+);
+router.get(
+  "/api/admin/patients/appointments/:date",
+  asyncHandler(
+    adminController.getPatientAppointmentsByDate.bind(adminController),
+  ),
 );
 
 module.exports = router;
