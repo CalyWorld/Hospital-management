30f01eed9e0a74ccfda1d0f5002920e98957854c
callistus <callistus.anizoba@lanexcorp.com>
2024-06-14 18:45:54 +0800

    implemented delete doctor feature

--- client/src/forms/DeleteDoctorForm.tsx
+++ client/src/forms/DeleteDoctorForm.tsx
@@ -1,24 +1,55 @@
+import { useAdminUser } from "../contexts/adminUserContext";
 import { TableProps } from "../pages/Doctor/DoctorsTable";
 
-export default function DeleteDoctor({ setActionForm }: TableProps) {
-  if (!setActionForm) return;
+export default function DeleteDoctor({
+  setActionForm,
+  selectedId,
+  setSelectedId,
+}: TableProps) {
+  if (!setActionForm || !setSelectedId) return;
+
+  const doctorId = selectedId;
+  const { useGetDoctorAndPatientData } = useAdminUser();
+  const { doctors, setDoctor } = useGetDoctorAndPatientData();
+  const doctor = doctors?.filter((doctor) => doctor._id === doctorId);
+
+  if (!doctor) return;
+  async function handleDoctorDelete() {
+    const selectedDoctor = doctors?.filter((doctor) => doctor._id !== doctorId);
+    if (!setActionForm || !setSelectedId || !setDoctor || !selectedDoctor)
+      return;
+    try {
+      await fetch(`http://localhost:3000/api/admin/doctor/${doctorId}/delete`, {
+        method: "DELETE",
+      });
+      setDoctor(selectedDoctor);
+      setActionForm("");
+      setSelectedId("");
+    } catch (error) {
+      console.error("Error deleting doctor:", error);
+    }
+  }
+
   return (
-    <div
-      className="fixed shadow-lg bg-[white] rounded-md  p-4 w-96"
-      style={{ top: "50%", left: "55%", transform: "translate(-50%, -50%)" }}
-    >
-      <h2 className="text-black text-lg font-bold mb-4">Delete User</h2>
+    <div className="shadow-lg bg-white rounded-md p-4 w-96">
+      <h2 className="text-black text-lg font-bold mb-4">{`Delete ${doctor[0]?.firstName} ${doctor[0]?.lastName}`}</h2>
       <p className="text-black mb-4">
         Are you sure you want to delete this user?
       </p>
       <div className="flex justify-end gap-2">
-        <button className="bg-darkBlue text-white py-2 px-4 rounded">
+        <button
+          className="bg-darkBlue text-white py-2 px-4 rounded"
+          onClick={() => {
+            handleDoctorDelete();
+          }}
+        >
           Delete
         </button>
         <button
           className="bg-darkBlue text-white py-2 px-4 rounded"
           onClick={() => {
             setActionForm("");
+            setSelectedId("");
           }}
         >
           Cancel
