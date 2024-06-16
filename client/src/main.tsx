dff743b3732b76457bdc5093bf37d2cf8f2ac17a
callistus <obinnaanizoba5@gmail.com>
2024-06-17 00:43:39 +0800

    moved provider to main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./input.css";
+import { AdminUserProvider } from "./contexts/adminUserContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AdminUserProvider>
+      <App />
+    </AdminUserProvider>
   </React.StrictMode>,
 );
