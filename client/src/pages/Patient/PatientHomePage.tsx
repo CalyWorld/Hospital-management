a4939463660c59d67a5efd475a730fbee07bf535
callistus <obinnaanizoba5@gmail.com>
2024-06-17 00:45:49 +0800

    called patient to props here

--- client/src/pages/Patient/PatientHomePage.tsx
+++ client/src/pages/Patient/PatientHomePage.tsx
@@ -1,12 +1,20 @@
 import { TableProps } from "../Doctor/DoctorsTable";
 import PatientsTable from "./PatientsTable";
 import { Outlet, useLocation } from "react-router";
-export default function PatientHomePage({ setActionForm }: TableProps) {
+export default function PatientHomePage({
+  setActionForm,
+  patients,
+  loading,
+}: TableProps) {
   const location = useLocation();
   return (
     <div className="m-10 shadow  rounded-md">
       {location.pathname === "/admin/patients" ? (
-        <PatientsTable setActionForm={setActionForm} />
+        <PatientsTable
+          setActionForm={setActionForm}
+          patients={patients}
+          loading={loading}
+        />
       ) : (
         <Outlet />
       )}
