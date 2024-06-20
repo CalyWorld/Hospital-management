574affcb84d8d55736d421926207a0534954a4e5
callistus <obinnaanizoba5@gmail.com>
2024-06-21 00:04:15 +0800

    actions for open patient and doctor form

---
+++ client/src/components/actionEnum.tsx
@@ -0,0 +1,10 @@
+export enum ActionDoctorEnum {
+  DeleteDoctorForm = "deleteDoctorForm",
+  EditDoctorForm = "editDoctorForm",
+  BookPatient = "bookPatient",
+}
+export enum ActionPatientEnum {
+  DeletePatientForm = "deletePatientForm",
+  EditPatientForm = "editPatientForm",
+  BookPatient = "bookPatient",
+}
