04ff0ee89a266deb8fca7077e9d3c4bc4a6a20af
callistus <obinnaanizoba5@gmail.com>
2024-06-20 16:27:09 +0800

    included background color here for layout

--- client/src/pages/Admin/AdminLayout.tsx
+++ client/src/pages/Admin/AdminLayout.tsx
@@ -11,7 +11,7 @@ export default function AdminLayout() {
   return (
     <nav
       id="sidebar"
-      className="w-48 h-full text-white flex flex-col gap-10 p-3"
+      className="bg-darkBlue w-48 h-full text-white flex flex-col gap-10 p-3"
     >
       <ul>
         <li>
