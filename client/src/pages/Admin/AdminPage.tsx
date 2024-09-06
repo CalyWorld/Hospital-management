81e5b2798d0d2158b3f8ed9fb2fab989cf81daec
callistus <obinnaanizoba5@gmail.com>
2024-09-07 07:30:05 +0800

    added responsiveness

--- client/src/pages/Admin/AdminPage.tsx
+++ client/src/pages/Admin/AdminPage.tsx
@@ -2,22 +2,33 @@ import { TableProps } from "../../components/tableProps";
 import AdminHeader from "./AdminHeader";
 import AdminLayout from "./AdminLayout";
 import { Outlet } from "react-router";
+import { TfiMenu } from "react-icons/tfi";
+import { useState } from "react";
 
 export default function AdminPage({ openActionForm }: TableProps) {
+  const [openResponsiveModal, setResponsiveModal] = useState<boolean>(false);
   const isFormOpen =
     openActionForm === "editDoctorForm" ||
     openActionForm === "editPatientForm" ||
     openActionForm === "deletePatientForm" ||
     openActionForm === "deleteDoctorForm" ||
     openActionForm === "bookPatient";
+
+  console.log(openResponsiveModal);
   return (
     <div className="flex h-screen">
       {isFormOpen && (
         <div className="absolute inset-0 z-10 backdrop-blur-sm bg-darkBlue/30"></div>
       )}
-      <div className="flex w-full h-full">
-        <div>
-          <AdminLayout />
+      <div className="flex w-full">
+        <div
+          className="flex items-center xs:block sm:block md:block lg:hidden p-2"
+          onClick={() => setResponsiveModal(true)}
+        >
+          <TfiMenu size={40} />
+        </div>
+        <div className="xs:hidden sm:hidden md:hidden lg:block">
+          <AdminLayout setResponsiveModal={setResponsiveModal} />
         </div>
         <div id="detail" className="w-full flex flex-col">
           <AdminHeader />
@@ -26,6 +37,13 @@ export default function AdminPage({ openActionForm }: TableProps) {
           </div>
         </div>
       </div>
+      {openResponsiveModal && (
+        <div className="absolute z-20 lg:hidden">
+          <div className="relative h-full">
+            <AdminLayout setResponsiveModal={setResponsiveModal} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
