83de0e837c09a15647bdfb547e9407ccc975dbcd
callistus <obinnaanizoba5@gmail.com>
2024-09-07 07:29:22 +0800

    called table props from the right file

--- client/src/pages/Admin/AdminHomePage.tsx
+++ client/src/pages/Admin/AdminHomePage.tsx
@@ -1,5 +1,5 @@
 import { useAdminUser } from "../../contexts/adminUserContext";
-import { TableProps } from "../Doctor/DoctorsTable";
+import { TableProps } from "../../components/tableProps";
 import AdminLoginPage from "./AdminLoginPage";
 import AdminPage from "./AdminPage";
 export default function AdminHomePage({ openActionForm }: TableProps) {
