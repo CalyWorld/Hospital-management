c930b8edd7d9b078df9cb9284c266623c659e30e
callistus <obinnaanizoba5@gmail.com>
2024-09-15 23:49:47 +0800

    removed contexts and passed redux store instead

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./input.css";
-import { AdminUserProvider } from "./contexts/adminUserContext.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AdminUserProvider>
+    <Provider store={store}>
       <App />
-    </AdminUserProvider>
+    </Provider>
   </React.StrictMode>,
 );
