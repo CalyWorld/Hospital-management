6677cec60834a572af3f66d3d4b329b33fa7a81d
callistus <obinnaanizoba5@gmail.com>
2024-09-15 23:48:02 +0800

    removed unnecessary props passed into component

--- client/src/pages/Doctor/DoctorHomePage.tsx
+++ client/src/pages/Doctor/DoctorHomePage.tsx
@@ -1,24 +1,10 @@
-import DoctorsTable, { TableProps } from "./DoctorsTable";
+import DoctorsTable from "./DoctorsTable";
 import { Outlet, useLocation } from "react-router";
-export default function DoctorHomePage({
-  setActionForm,
-  setSelectedId,
-  doctors,
-  loading,
-}: TableProps) {
+export default function DoctorHomePage() {
   const location = useLocation();
   return (
     <div className="m-10 shadow rounded-md">
-      {location.pathname === "/admin/doctors" ? (
-        <DoctorsTable
-          setActionForm={setActionForm}
-          setSelectedId={setSelectedId}
-          doctors={doctors}
-          loading={loading}
-        />
-      ) : (
-        <Outlet />
-      )}
+      {location.pathname === "/admin/doctors" ? <DoctorsTable /> : <Outlet />}
     </div>
   );
 }
