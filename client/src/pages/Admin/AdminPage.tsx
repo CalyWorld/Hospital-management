30752ecc71a2412c352489a47a6ee508bd9cdc16
callistus <obinnaanizoba5@gmail.com>
2024-09-15 23:47:28 +0800

    rewrote and used redux

--- client/src/pages/Admin/AdminPage.tsx
+++ client/src/pages/Admin/AdminPage.tsx
@@ -1,42 +1,41 @@
-import { TableProps } from "../../components/tableProps";
 import AdminHeader from "./AdminHeader";
 import AdminLayout from "./AdminLayout";
 import { Outlet } from "react-router";
 import { TfiMenu } from "react-icons/tfi";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store";
 
-export default function AdminPage({ openActionForm }: TableProps) {
+export default function AdminPage() {
   const [openResponsiveModal, setResponsiveModal] = useState<boolean>(false);
-  const isFormOpen =
-    openActionForm === "editDoctorForm" ||
-    openActionForm === "editPatientForm" ||
-    openActionForm === "deletePatientForm" ||
-    openActionForm === "deleteDoctorForm" ||
-    openActionForm === "bookPatient";
+  const actionForm = useSelector(
+    (state: RootState) => state.actionForm.openActionForm,
+  );
 
-  console.log(openResponsiveModal);
   return (
-    <div className="flex h-screen">
-      {isFormOpen && (
+    <div className="flex h-screen overflow-hidden">
+      {actionForm && (
         <div className="fixed inset-0 z-10 backdrop-blur-sm bg-darkBlue/30"></div>
       )}
-      <div className="flex w-full">
-        <div
-          className="lg:hidden p-2 flex"
-          onClick={() => setResponsiveModal(true)}
-        >
-          <TfiMenu size={40} />
+      <div className="flex w-full min-h-full">
+        {/* Sidebar */}
+        <div className="lg:hidden p-2 flex">
+          <TfiMenu size={40} onClick={() => setResponsiveModal(true)} />
         </div>
-        <div className="hidden lg:block">
+        <div className="hidden lg:block flex-shrink-0">
           <AdminLayout setResponsiveModal={setResponsiveModal} />
         </div>
-        <div id="detail" className="w-full flex flex-col">
+
+        {/* Content section */}
+        <div id="detail" className="flex-grow flex flex-col overflow-hidden">
           <AdminHeader />
-          <div className="flex-grow overflow-y-auto">
+          <div className="flex-grow p-4 overflow-auto max-w-full">
             <Outlet />
           </div>
         </div>
       </div>
+
+      {/* Responsive Modal for Sidebar */}
       {openResponsiveModal && (
         <div className="fixed inset-0 z-20 flex">
           <div className="w-48 bg-darkBlue">
