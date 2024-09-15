afc8ce96a1d92a5a28179bd2380f4ce0bb2f32ee
callistus <obinnaanizoba5@gmail.com>
2024-09-15 23:48:46 +0800

    used redux to get admin user

--- client/src/pages/Admin/AdminHomePage.tsx
+++ client/src/pages/Admin/AdminHomePage.tsx
@@ -1,12 +1,10 @@
-import { useAdminUser } from "../../contexts/adminUserContext";
-import { TableProps } from "../../components/tableProps";
 import AdminLoginPage from "./AdminLoginPage";
 import AdminPage from "./AdminPage";
-export default function AdminHomePage({ openActionForm }: TableProps) {
-  const { adminUser } = useAdminUser();
-  return adminUser ? (
-    <AdminPage openActionForm={openActionForm} />
-  ) : (
-    <AdminLoginPage />
+import { RootState } from "../../redux/store";
+import { useSelector } from "react-redux";
+export default function AdminHomePage() {
+  const adminUser = useSelector(
+    (state: RootState) => state.adminUser.adminUser,
   );
+  return adminUser ? <AdminPage /> : <AdminLoginPage />;
 }
