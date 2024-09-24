3af82d032a36b5b014bc5fda8dec4b35f772f952
callistus <obinnaanizoba5@gmail.com>
2024-09-25 02:37:24 +0800

    implemented store so i can get my redux states and actions

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import adminUserReducer from "./adminUserSlice";
 import doctorAndPatientReducer from "../redux/doctorAndPatientSlice";
+import doctorAndPatientDetailReducer from "../redux/doctorAndPatientDetailsSlice";
+import doctorAndPatientAppointmentsReducer from "../redux/appointmentsSlice";
+import doctorAndPatientTreatmentsReducer from "../redux/treamentSlice";
+import healthRecordsReducer from "../redux/healthRecordsSlice";
 import actionFormReducer from "../redux/actionFormSlice";
 const store = configureStore({
   reducer: {
     adminUser: adminUserReducer,
     doctorAndPatientUser: doctorAndPatientReducer,
+    doctorAndPatientDetail: doctorAndPatientDetailReducer,
+    doctorAndPatientAppointments: doctorAndPatientAppointmentsReducer,
+    doctorAndPatientTreatments: doctorAndPatientTreatmentsReducer,
+    healthRecords: healthRecordsReducer,
     actionForm: actionFormReducer,
   },
 });
