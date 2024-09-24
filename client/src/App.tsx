92f8b7d51546462cde29533e6229863751d18481
callistus <obinnaanizoba5@gmail.com>
2024-09-25 02:48:18 +0800

    update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,48 +1,31 @@
 import DoctorHomePage from "./pages/Doctor/DoctorHomePage";
 import PatientHomePage from "./pages/Patient/PatientHomePage";
 import AdminHomePage from "./pages/Admin/AdminHomePage";
-import { useState } from "react";
-import { useAdminUser } from "./contexts/adminUserContext";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./errorPage";
 import AdminDashBoard from "./pages/Admin/AdminDashBoard";
-import { DoctorDetails } from "./pages/Doctor/DoctorDetails";
-import PatientsTable from "./pages/Patient/PatientsTable";
-import { PatientDetails } from "./pages/Patient/PatientDetails";
-import DoctorsTable from "./pages/Doctor/DoctorsTable";
 import EditDoctorDetail from "./forms/EditDoctorDetailsForm";
 import DeleteDoctor from "./forms/DeleteDoctorForm";
 import BookAppointment from "./forms/BookAppointment";
+import { AppDispatch } from "./redux/store";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "./redux/store";
+import { resetActionForm } from "./redux/actionFormSlice";
+import { PatientDetails } from "./pages/Patient/PatientDetails";
+import { DoctorDetails } from "./pages/Doctor/DoctorDetails";
 import DoctorScheduledAppointmentTable from "./pages/Doctor/DoctorScheduledAppointmentTable";
 import DoctorCompletedAppointmentTable from "./pages/Doctor/DoctorCompletedAppointmentTable";
-import PatientScheduledAppointmentTable from "./pages/Patient/PatientScheduledAppointmentTable";
-import PatientCompletedAppointmentTable from "./pages/Patient/PatientCompletedAppointmentTable";
+import PatientsTable from "./pages/Patient/PatientsTable";
 
-function RouterFunc() {
-  const [openActionForm, setActionForm] = useState<string>("");
-  const [selectedId, setSelectedId] = useState<string>("");
-  const { useGetDoctorAndPatientData } = useAdminUser();
-  const { doctors, setDoctor, loading, patients } =
-    useGetDoctorAndPatientData();
-
-  // Check if data is still loading or undefined
-  if (!doctors || !patients)
-    return {
-      router: undefined,
-      openActionForm,
-      setActionForm,
-      selectedId,
-      setSelectedId,
-      doctors: [],
-      patients: [],
-      setDoctor,
-      loading,
-    };
-
+function App() {
+  const dispatch: AppDispatch = useDispatch();
+  const { openActionForm, selectedId } = useSelector(
+    (state: RootState) => state.actionForm,
+  );
   const router = createBrowserRouter([
     {
       path: "/admin",
-      element: <AdminHomePage openActionForm={openActionForm} />,
+      element: <AdminHomePage />,
       errorElement: <ErrorPage />,
       children: [
         {
@@ -54,71 +37,21 @@ function RouterFunc() {
           element: <AdminDashBoard />,
           children: [
             {
-              path: "doctor/:doctorId",
+              path: "doctor/:doctorId/*",
               element: <DoctorDetails />,
-              children: [
-                {
-                  index: true,
-                  path: "active",
-                  element: <DoctorScheduledAppointmentTable />,
-                },
-                {
-                  path: "completion",
-                  element: <DoctorCompletedAppointmentTable />,
-                },
-                {
-                  path: "patients",
-                  element: (
-                    <PatientsTable
-                      setActionForm={setActionForm}
-                      patients={patients}
-                      loading={loading}
-                    />
-                  ),
-                },
-              ],
             },
             {
-              path: "patient/:patientId",
+              path: "patient/:patientId/*",
               element: <PatientDetails />,
-              children: [
-                {
-                  index: true,
-                  path: "active",
-                  element: <PatientScheduledAppointmentTable />,
-                },
-                {
-                  path: "completion",
-                  element: <PatientCompletedAppointmentTable />,
-                },
-                {
-                  path: "patients",
-                  element: (
-                    <DoctorsTable
-                      setActionForm={setActionForm}
-                      setSelectedId={setSelectedId}
-                      doctors={doctors}
-                      loading={loading}
-                    />
-                  ),
-                },
-              ],
             },
           ],
         },
         {
           path: "doctors",
-          element: (
-            <DoctorHomePage
-              setActionForm={setActionForm}
-              setSelectedId={setSelectedId}
-              doctors={doctors}
-              loading={loading}
-            />
-          ),
+          element: <DoctorHomePage />,
           children: [
             {
-              path: "doctor/:doctorId",
+              path: "doctor/:doctorId/*",
               element: <DoctorDetails />,
               children: [
                 {
@@ -130,55 +63,18 @@ function RouterFunc() {
                   path: "completion",
                   element: <DoctorCompletedAppointmentTable />,
                 },
-                {
-                  path: "patients",
-                  element: (
-                    <PatientsTable
-                      setActionForm={setActionForm}
-                      patients={patients}
-                      loading={loading}
-                    />
-                  ),
-                },
+                { path: "patients", element: <PatientsTable /> },
               ],
             },
           ],
         },
         {
           path: "patients",
-          element: (
-            <PatientHomePage
-              setActionForm={setActionForm}
-              patients={patients}
-              loading={loading}
-            />
-          ),
+          element: <PatientHomePage />,
           children: [
             {
-              path: "patient/:patientId",
+              path: "patient/:patientId/*",
               element: <PatientDetails />,
-              children: [
-                {
-                  index: true,
-                  path: "active",
-                  element: <PatientScheduledAppointmentTable />,
-                },
-                {
-                  path: "completion",
-                  element: <PatientCompletedAppointmentTable />,
-                },
-                {
-                  path: "doctors",
-                  element: (
-                    <DoctorsTable
-                      setActionForm={setActionForm}
-                      setSelectedId={setSelectedId}
-                      doctors={doctors}
-                      loading={loading}
-                    />
-                  ),
-                },
-              ],
             },
           ],
         },
@@ -186,42 +82,18 @@ function RouterFunc() {
     },
   ]);
 
-  return {
-    router,
-    openActionForm,
-    setActionForm,
-    selectedId,
-    setSelectedId,
-    doctors,
-    patients,
-    setDoctor,
-    loading,
+  const handleClose = () => {
+    dispatch(resetActionForm());
   };
-}
 
-function App() {
-  const {
-    router,
-    openActionForm,
-    setActionForm,
-    selectedId,
-    setSelectedId,
-    doctors,
-    patients,
-    setDoctor,
-    loading,
-  } = RouterFunc();
-
-  if (!router) return;
   return (
-    <div className="relative">
+    <div className="w-full flex flex-col flex-grow min-h-full">
       <RouterProvider router={router} />
       {openActionForm && (
         <div
           className="fixed inset-0 z-50 flex items-center justify-center"
           onClick={() => {
-            setActionForm("");
-            setSelectedId("");
+            handleClose();
           }}
         >
           <div
@@ -229,23 +101,13 @@ function App() {
             onClick={(e) => e.stopPropagation()}
           >
             {openActionForm === "editDoctorForm" && selectedId && (
-              <EditDoctorDetail
-                setActionForm={setActionForm}
-                selectedId={selectedId}
-                setSelectedId={setSelectedId}
-                doctors={doctors}
-                setDoctor={setDoctor}
-                loading={loading}
-              />
+              <EditDoctorDetail selectedId={selectedId} />
             )}
             {openActionForm === "deleteDoctorForm" && selectedId && (
-              <DeleteDoctor
-                selectedId={selectedId}
-                setActionForm={setActionForm}
-                setSelectedId={setSelectedId}
-                doctors={doctors}
-                setDoctor={setDoctor}
-              />
+              <DeleteDoctor selectedId={selectedId} />
+            )}
+            {openActionForm === "bookPatient" && selectedId && (
+              <BookAppointment selectedId={selectedId} />
             )}
             {/* {openActionForm === "editPatientForm" && selectedId && (
             <EditDoctorDetail
@@ -266,9 +128,6 @@ function App() {
               setDoctor={setDoctor}
             />
           )} */}
-            {openActionForm === "bookPatient" && selectedId && (
-              <BookAppointment setActionForm={setActionForm} />
-            )}
           </div>
         </div>
       )}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      xs: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     colors: {
       purple: "#701a75",
       white: "#f8fafc",
